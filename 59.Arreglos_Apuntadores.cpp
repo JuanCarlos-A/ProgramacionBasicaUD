b00ceeacbeb9ac89757648d540f3e1159ca3d426
Juan_Carlos_Programmer <133262668+JuanCarlos-A@users.noreply.github.com>
2025-12-01 23:14:27 -0500

    Update Apuntadores

--- 59.Arreglos_Apuntadores.cpp
+++ 59.Arreglos_Apuntadores.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
 	int A[10];
 	int numero = 0;
+	int *ptr = A;
 	
 	for (int i = 0; i < 10; i++){
 		cout << "Ingresa el valor del arreglo en la posicion " << i+1 << ": ";
@@ -18,6 +19,16 @@ int main(){
 	}
 	
 	//Segundo tipo
+
+	for (int i = 0; i < 10; i++){
+		cout << "-" << *(ptr + i) << "- "; 
+	}
+
+	// Tercer tipo
+	for (int i = 0; i < 10; i++){
+		cout << "-" << *(A + i) << "- "; 
+	}	
+	return 0;
 	
 	
 }
